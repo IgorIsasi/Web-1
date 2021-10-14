c7f86721c1afeb607c21c39804f25b2a650fe79c
Marru11 <markelmontejo@gmail.com>
2021-10-14 21:44:39 +0200

    Nuevos cambios en js

--- app/datuakEgiaztatu.js
+++ app/datuakEgiaztatu.js
@@ -1,100 +1,52 @@
 const formulario = document.getElementById('idFormulario');
-const inputs = document.querySelectorAll('#formulario input');
+const inputs = document.querySelectorAll('#idFormulario input');
 
-
-const expresiones = {
-	izena: /^[a-zA-ZÀ-ÿ\s]{8,25}$/, // Letras y espacios, pueden llevar acentos.
+const espresioak = {
+    Nan: /^\d{8}[A-Z]$/, // 7 a 14 numeros.
+	izenaAbizena: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	pasahitza: /^.{4,12}$/, // 4 a 12 digitos.
 	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefonoa: /^\d{9}$/, // 7 a 14 numeros.
-        NAN: /^\d{8}[A-Z]{1}$/
-
+	telefonoa: /^\d{9}$/ // 7 a 14 numeros.
 }
+const validarFormulario=(e)=>{
+    switch(e.target.name){
+        case "idIzena":
+            if(espresioak.izenaAbizena.test(e.target.value)){
 
-
-
-function datuakEgiaztatuLogin(){
-   var email = document.login.email.value;
-   var pasahitza = document.login.pasahitza.value;
-
-   if (email.length <= 0){
-            window.alert("Ez duzu email-a sartu");
-   }
-   else if (pasahitza.length <= 0) {
-            window.alert("Ez duzu pasahitza sartu");
-   }
-   else { 
-           document.login.submit();
-   }
-}
-
-function datuakEgiaztatuErregistroa(){
+            }else{
+                document.getElementById('idIzena').classList.add()
+            }
         
+        break;        
+        case "idAbizena":
+        
+        break;
+        case "idNan":
+        
+        break;
+        case "idTelefonoa":
+        
+        break;
+        case "idJaiotzeData":
+        
+        break;
+        case "idEmail":
+        
+        break;
+        case "idPasahitza":
+        
+        break;
 
-
-   var izena = document.getElementById('idIzena');
-   var abizenak = document.getElementById('idAbizena');
-   var NAN = document.getElementById('idNan');
-   var telefonoa = document.getElementById('idTelefonoa');
-   var jaiotzeData = document.getElementById('idJaiotzeData');
-   var email = document.getElementById('idEmail');
-   var pasahitza = document.getElementById('idPasahitza');
-
-
-
-   if (izena.length <= 0){
-           
-   }
-   else if (abizenak.length <= 0){
-           window.alert("Abizenak sartu");
-   }
-   else if (NAN.length <= 0){
-           window.alert("NAN zenbakia sartu");
-   }
-   else if (telefonoa.length <= 0){
-           window.alert("Telefono zenbakia sartu");
-   }
-   else if (jaiotzeData.length <= 0){
-           window.alert("Jaiotze data sartu");
-   }
-   else if(email.length <= 0){
-           window.alert("Email-a sartu");
-   }
-   else if(pasahitza.length <= 0) {
-           window.alert("Pasahitza sartu");
-   }
-   else{
-           document.erregistroa.submit();
-   }       
+    }
+    
 }
 
-function datuakEgiaztatuLiburuaSartu(){
-        var izena = document.liburuaSartu.izena.value;
-        var egilea = document.liburuaSartu.egilea.value;
-        var orriKop = document.liburuaSartu.orriKop.value;
-        var prezioa = document.liburuaSartu.prezioa.value;
-        var stock = document.liburuaSartu.stock.value;
-        var irudia = document.liburuaSartu.irudia.value;
-     
-        if (izena.length <= 0){
-                window.alert("Izena sartu");
-        }
-        else if (egilea.length <= 0){
-                window.alert("Egilea sartu");
-        }
-        else if (orriKop.length <= 0){
-                window.alert("Orri kopurua sartu");
-        }
-        else if (prezioa.length <= 0){
-                window.alert("Prezioa sartu");
-        }
-        else if (stock.length <= 0){
-                window.alert("Liburu kopurua sartu");
-        }
-        else if(irudia.length <= 0){
-                window.alert("Irudia sartu");
-        }
-        else{
-                document.liburuaSartu.submit();
-        }       
-}
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup',validarFormulario);
+    input.addEventListener('blur',validarFormulario);
+})
+
+formulario.addEventListener('submit', (e)=>{
+    e.preventDefault(); //Para evitar que sea enviado
+})
