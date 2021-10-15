26fd02621e91c2e42844f4806ac860d45d4b8975
Marru11 <46656741+Marru11@users.noreply.github.com>
2021-10-15 17:52:17 +0200

    Funcionan todas las validaciones

--- app/datuakEgiaztatu.js
+++ app/datuakEgiaztatu.js
@@ -9,31 +9,56 @@ const espresioak = {
 	telefonoa: /^\d{9}$/, // 7 a 14 numeros.
     jaiotzeData: /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/
 }
-const validarFormulario=(e)=>{
+const erregistroaOnartu=(e)=>{
     switch(e.target.name){
         case "izenAbizen":
-            validarCampo(espresioak.izenAbizen, e.target, 'izenAbizen');
+            hutsuneaOnartu(espresioak.izenAbizen, e.target, 'izenAbizen');
         break;        
         case "nan":
-            validarCampo(espresioak.nan, e.target, 'nan');
+            hutsuneaOnartu(espresioak.nan, e.target, 'nan');
         break;
         case "pasahitza":
-            validarCampo(espresioak.pasahitza, e.target, 'pasahitza');        
+            hutsuneaOnartu(espresioak.pasahitza, e.target, 'pasahitza');        
+        break;
+        case "pasahitza2": 
+        //Kasu honetan pasahitza errepikatua ez du beste inputen era berean jokatzen
+            pasahitza2Onartu();
+                    
         break;
         case "telefonoa":
-            validarCampo(espresioak.telefonoa, e.target, 'telefonoa');
+            hutsuneaOnartu(espresioak.telefonoa, e.target, 'telefonoa');
         break;
         case "jaiotzeData":
-            validarCampo(espresioak.jaiotzeData, e.target, 'jaiotzeData');
+            hutsuneaOnartu(espresioak.jaiotzeData, e.target, 'jaiotzeData');
         break;
         case "email":
-            validarCampo(espresioak.email, e.target, 'email')
+            hutsuneaOnartu(espresioak.email, e.target, 'email');
         break;
     }
     
 }
 
-const validarCampo=(expresion,input,campo)=>{
+const pasahitza2Onartu=()=>{
+    const inputPasahitza1=document.getElementById('pasahitza');
+    const inputPasahitza2=document.getElementById('pasahitza2');
+    if(inputPasahitza1.value==inputPasahitza2.value){
+        document.getElementById(`grupo__pasahitza2`).classList.remove('formulario__grupo-incorrecto');
+        document.querySelector(`#grupo__pasahitza2 i`).classList.remove('fa-times-circle');
+        document.getElementById(`grupo__pasahitza2`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__pasahitza2 i`).classList.add('fa-check-circle');            
+        document.querySelector(`#grupo__pasahitza2 .formulario__input-error`).classList.remove('formulario__input-error-activo')
+    }else{
+        document.getElementById(`grupo__pasahitza2`).classList.add('formulario__grupo-incorrecto');
+        document.querySelector(`#grupo__pasahitza2 i`).classList.add('fa-times-circle');
+        document.getElementById(`grupo__pasahitza2`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__pasahitza2 i`).classList.remove('fa-check-circle');            
+        document.querySelector(`#grupo__pasahitza2 .formulario__input-error`).classList.add('formulario__input-error-activo')
+    }
+
+
+}
+
+const hutsuneaOnartu=(expresion,input,campo)=>{
     if(expresion.test(input.value)){
         document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
         document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
@@ -53,8 +78,8 @@ const validarCampo=(expresion,input,campo)=>{
 inputs.forEach((input)=>{ 
     //Zerbai input batean aldatzen bada validarFormulario() funtzioa
     //exekutatuko da. Baita input-aren kanpoan klik egiten bada.
-    input.addEventListener('keyup',validarFormulario);
-    input.addEventListener('blur',validarFormulario);
+    input.addEventListener('keyup',erregistroaOnartu);
+    input.addEventListener('blur',erregistroaOnartu);
 })
 
 formulario.addEventListener('submit', (e)=>{
