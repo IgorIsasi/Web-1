5d5bf12eb00def0072cd7f6284e0d79bef5e4af6
Marru11 <46656741+Marru11@users.noreply.github.com>
2021-10-18 07:00:57 +0200

    He metido el login nuevo

--- app/loginDB.php
+++ app/loginDB.php
@@ -4,10 +4,15 @@
   $email = $_POST['email'];
   $pasahitza = $_POST['pasahitza'];
 
+  echo $email;
+  echo $pasahitza;
+
   $zutabekop = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM usuarios WHERE email = '$email' AND pasahitza = '$pasahitza'"));
 
 if($zutabekop <= 0){
-  echo("<script>alert('Erabiltzaile edo pasahitza okerra!')</script>");
+  echo $email;
+  echo $pasahitza;
+  echo("AAA<script>alert('Erabiltzaile edo pasahitza okerra!')</script>");
   echo("<script>window.location = 'index.html;;</script>");
 }
 else{
