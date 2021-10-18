5b2209e164ff13d0275fddd5957b4c1694b83d02
Marru11 <markelmontejo@gmail.com>
2021-10-18 18:11:42 +0200

    Quitado el error de no header

--- app/saioaHasi/loginDB.php
+++ app/saioaHasi/loginDB.php
@@ -1,20 +1,21 @@
+<?php //Para evitar que de problemas con el header
+ob_start();
+?>
+
 <?php
   include '../config.php';
-
   $email = $_POST['email'];
   $pasahitza = $_POST['pasahitza'];
-
-
   $emaitza = mysqli_query($conn,"SELECT * FROM usuarios WHERE email = '$email' AND pasahitza = '$pasahitza'");
   $zutabekop= mysqli_num_rows($emaitza);
   echo $zutabekop;
 if($zutabekop <= 0){
-  echo $email;
-  echo $pasahitza;
-  echo("AAA<script>alert('Erabiltzaile edo pasahitza okerra!')</script>");
   echo("<script>window.location = 'index.html;;</script>");
 }else{
   header('Location: ../web/web.php');
-  exit;
 }
+?>
+
+<?php //Para evitar que de problemas con el header
+ob_end_flush();
 ?>
